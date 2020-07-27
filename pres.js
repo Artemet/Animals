058d03d46e07cc8965cc600553bc3a13a588f5a4
iMac 21 <imac21@iMac-iMac.local>
2020-07-27 22:06:19 +0300

    Animals

---
+++ pres.js
@@ -0,0 +1,51 @@
+function tigers(){
+    alert("This is a tiger")
+}
+
+function panda(){
+    alert("This is a panda")
+}
+
+function ber(){
+    alert("This is a ber")
+}
+
+function pingvin(){
+    alert("This is a pingvin")
+}
+
+function zebra(){
+    alert("This is a zebra")
+}
+
+function elephant(){
+    alert("This is a elephant")
+}
+
+function wolf(){
+    alert("This is a wolf")
+}
+
+function dog(){
+    alert("This is a dog")
+}
+
+function cat(){
+    alert("This is a cat")
+}
+
+function hippo(){
+    alert("This is a hippo")
+}
+
+function giraffe(){
+    alert("This is a giraffe")
+}
+
+function fox(){
+    alert("This is a fox")
+}
+
+function squirrel(){
+    alert("This is a squirrel")
+}
